15375725395b40568ad855ee7969dffa6e842ea9
CezzMartins <cezzmartins@gmail.com>
2022-07-04 19:57:16 -0300

    feat: models base

---
+++ LeaveMenagement.Web/Data/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveMenagement.Web.Data
+{
+    public class LeaveType : BaseEntity
+    {
+        public string Name { get; set; }
+        public int DefaultDauys { get; set; }
+
+    }
+}
